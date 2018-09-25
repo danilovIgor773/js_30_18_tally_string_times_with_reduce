0526a26a3eb69b1b442c1360099a8f7a1d0dc34c
Danilov Igor <danilovigor773@gmail.com>
2018-09-25 19:44:50 +0500

    Finished 18-th lesson of JS 30

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './index.scss';
 
-console.log("Hello World");
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes
+  .map(node => node.dataset.time)
+  .map(timeCode => {
+    const [mins, secs] = timeCode.split(':').map(parseFloat);
+    return (mins*60) + secs;
+    //console.log(mins, secs);
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds);
+
+  let secondsLeft = seconds;
+  const hours = Math.floor(secondsLeft / 3600);
+  secondsLeft = secondsLeft % 3600;
+
+  const mins = Math.floor(secondsLeft / 60);
+  secondsLeft = secondsLeft % 60;
+
+
+console.log("total is : ", seconds);
+console.log("hours : ", hours);
+console.log("minutes : ", mins);
+console.log("seconds left : ", secondsLeft);
